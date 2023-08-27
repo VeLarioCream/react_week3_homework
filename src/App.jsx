e1af2e420501f07bfa5088f36e96d128ce0c34c2
VeLairoCream <ysh523h@hotmail.com>
2023-08-27 18:03:48 +0800

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const apiURL = {
@@ -14,14 +14,24 @@ const apiURL = {
 function App() {
   const [count, setCount] = useState(0)
   const [token, setToken] = useState('')
+  const [nickName, setNickName] = useState('')
 
   return (
     <>
-      <SignUp />
-      <SignIn token={token} setToken={setToken}/>
-      <CheckOut token={token} setToken={setToken}/>
-      <SignOut token={token} setToken={setToken}/>
-      {token && <TodoList token={token}/>}
+      <div className="container my-5 overflow-hidden">
+        <div className="row gx-3">
+          <div className="col-6">
+            <SignUp />
+            <SignIn setToken={setToken} setNickName={setNickName} />
+            <CheckOut token={token} setToken={setToken} />
+            <SignOut token={token} setToken={setToken} />
+          </div>
+          <div className="container col-6">
+            {token ? <TodoList token={token} nickName={nickName} /> :
+              <><div className="container my-4 mx-3"><h1><span className="badge badge-secondary">《代辦事項清單》</span></h1><div className="alert alert-secondary ml-3" role="alert">  請先登入！ </div></div></>}
+          </div>
+        </div>
+      </div>
     </>
   )
 }
@@ -53,7 +63,7 @@ function SignUp() {
         註冊成功【status】 -> ${res.status}
         註冊成功【message】 -> UID： ${res.data.uid}
       `)
-      console.dir(res);
+      // console.dir(res);
     } catch (error) {
       setMsg(`
         註冊失敗【error】 -> ${error}  
@@ -87,7 +97,7 @@ function SignUp() {
           </div>
           <button type="button" className="btn btn-primary ml-3" onClick={handleSubmit}>註冊</button>
         </form>
-        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whitSpace": "pre-line", "overflowWrap" : "break-word" }}>
+        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whitSpace": "pre-line", "overflowWrap": "break-word" }}>
           執行結果：<br />{msg}
         </div>
         <hr />
@@ -96,7 +106,7 @@ function SignUp() {
   )
 }
 
-function SignIn({token, setToken}) {
+function SignIn({ setToken, setNickName }) {
 
   // console.log(apiURL.baseURL + apiURL.signUpPath);
   const [email, setEmail] = useState('')
@@ -117,10 +127,13 @@ function SignIn({token, setToken}) {
     try {
       const res = await axios.post(apiURL.baseURL + apiURL.sighInPath, formData)
       setToken(res.data.token)
+      setNickName(res.data.nickname)
       setMsg(`
       登入成功【status】 -> ${res.status}
       登入成功【Token】 -> 
       ${res.data.token}`)
+      // console.dir(res)
+      // console.log(nickName)
     } catch (error) {
       setMsg(`
       登入失敗【error】 -> ${error}     
@@ -148,7 +161,7 @@ function SignIn({token, setToken}) {
           </div>
           <button type="button" className="btn btn-primary ml-3" onClick={handleSubmit}>登入</button>
         </form>
-        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap" : "break-word" }}>
+        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap": "break-word" }}>
           執行結果：<br /> {msg}
         </div>
         <hr />
@@ -157,7 +170,7 @@ function SignIn({token, setToken}) {
   )
 }
 
-function CheckOut({token, setToken}) {
+function CheckOut({ token, setToken }) {
 
   // console.log(apiURL.baseURL + apiURL.signUpPath);
   const [msg, setMsg] = useState('')
@@ -194,15 +207,15 @@ function CheckOut({token, setToken}) {
       <div className="container my-4 mx-3">
         <h1><span className="badge badge-secondary">《驗證》</span></h1>
         <form>
-        <div className="form-group ml-3">
+          <div className="form-group ml-3">
             <label htmlFor="token">Token：</label>
             <input type="text" className="form-control" id="checkOutToken" value={token} placeholder="請輸入Token" onChange={
               (e) => { setToken(e.target.value) }
             } />
-          </div>         
+          </div>
           <button type="button" className="btn btn-primary ml-3" onClick={handleSubmit}>驗證</button>
         </form>
-        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap" : "break-word" }}>
+        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap": "break-word" }}>
           執行結果：<br /> {msg}
         </div>
         <hr />
@@ -211,7 +224,7 @@ function CheckOut({token, setToken}) {
   )
 }
 
-function SignOut({token, setToken}) {
+function SignOut({ token, setToken }) {
 
   // console.log(apiURL.baseURL + apiURL.signUpPath);
   const [msg, setMsg] = useState('')
@@ -229,7 +242,7 @@ function SignOut({token, setToken}) {
       return
     }
     try {
-      const res = await axios.post(apiURL.baseURL + apiURL.signOutPath, {} , formHeaders)
+      const res = await axios.post(apiURL.baseURL + apiURL.signOutPath, {}, formHeaders)
       setToken('')
       setMsg(`
         登出成功【status】 -> ${res.status}
@@ -249,15 +262,15 @@ function SignOut({token, setToken}) {
       <div className="container my-4 mx-3">
         <h1><span className="badge badge-secondary">《登出》</span></h1>
         <form>
-        <div className="form-group ml-3">
+          <div className="form-group ml-3">
             <label htmlFor="token">Token：</label>
             <input type="text" className="form-control" id="sighOutToken" value={token} placeholder="請輸入Token" onChange={
               (e) => { setToken(e.target.value) }
             } />
-          </div>         
+          </div>
           <button type="button" className="btn btn-primary ml-3" onClick={handleSubmit}>登出</button>
         </form>
-        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap" : "break-word" }}>
+        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap": "break-word" }}>
           執行結果：<br /> {msg}
         </div>
         <hr />
@@ -266,10 +279,189 @@ function SignOut({token, setToken}) {
   )
 }
 
-function TodoList ({token}) {
+function TodoList({ token, nickName }) {
+  const [msg, setMsg] = useState('')
+  const [todo, setTodo] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+  const [editId, setEditId] = useState('')
+  const [newContent, setNewContent] = useState('')
+
+  const headers = {
+    "headers": {
+      authorization: token
+    }
+  }
+
+  const addPayload = {
+    "content": newTodo
+  }
+
+  const updatePayload = {
+    "content": newContent
+  }
+
+  useEffect(() => {
+    getTodoList()
+  }, [token])
+
+  async function getTodoList() {
+    try {
+      const res = await axios.get(apiURL.baseURL + apiURL.todoPath, headers)
+      setMsg(`
+        清單讀取成功【status】 -> ${res.status}`)
+      setTodo(res.data.data)
+    } catch (error) {
+      console.dir(error)
+      setMsg(`
+          清單讀取失敗【error】 -> ${error.message}     
+          清單讀取失敗【message】 -> ${error.response.data.message}
+        `)
+    }
+  }
+
+  async function addTodoList() {
+    try {
+      const res = await axios.post(apiURL.baseURL + apiURL.todoPath, addPayload, headers)
+      setNewTodo('')
+      setMsg(`
+        新增成功【status】 -> ${res.status}
+        新增成功【content】 -> 「${res.data.newTodo.content}」 已加入待辦清單
+        `)
+      getTodoList()
+      console.dir(res.data.newTodo.content)
+
+    } catch (error) {
+      console.dir(error)
+      setMsg(`
+          新增失敗【error】 -> ${error.message}     
+          新增失敗【message】 -> ${error.response.data.message}
+        `)
+    }
+  }
+
+  function handleAddTodo() {
+    addTodoList()
+  }
+
+  async function delTodoList(id) {
+    try {
+      const res = await axios.delete(apiURL.baseURL + apiURL.todoPath + id, headers)
+      setMsg(`
+        刪除成功【status】 -> ${res.status}
+        刪除成功【content】 -> ${res.data.message}
+        `)
+      getTodoList()
+      // console.dir(res)
+
+    } catch (error) {
+      // console.dir(error)
+      setMsg(`
+          刪除失敗【error】 -> ${error.message}     
+          刪除失敗【message】 -> ${error.response.data.message}
+        `)
+    }
+  }
+
+  function handleDelete(id) {
+    delTodoList(id)
+  }
+
+  async function updateTodoList(id, newContent) {
+    try {
+      const res = await axios.put(apiURL.baseURL + apiURL.todoPath + id, updatePayload, headers)
+      setNewContent('')
+      setMsg(`
+        修改成功【status】 -> ${res.status}
+        修改成功【content】 -> ${res.data.message}
+        `)
+      getTodoList()
+      // console.dir(res)
+
+    } catch (error) {
+      // console.dir(error)
+      setMsg(`
+          修改失敗【error】 -> ${error.message}     
+          修改失敗【message】 -> ${error.response.data.message}
+        `)
+    }
+  }
+
+  function handleUpdate(id, newContent) {
+    updateTodoList(id, newContent)
+  }
+
+  async function toggleTodoList(id) {
+    try {
+      const res = await axios.patch(apiURL.baseURL + apiURL.todoPath + id + '/toggle', {}, headers)
+      setMsg(`
+        變更成功【status】 -> ${res.status}
+        變更成功【content】 -> ${res.data.message}
+        `)
+      getTodoList()
+      // console.dir(res)
+
+    } catch (error) {
+      // console.dir(error)
+      setMsg(`
+          變更失敗【error】 -> ${error.message}     
+          變更失敗【message】 -> ${error.response.data.message}
+        `)
+    }
+  }
+
+  function handleToggle(id) {
+    toggleTodoList(id)
+  }
+
   return (
     <>
-      <div>123</div>
+      <div className="container my-4 mx-3">
+        <h1><span className="badge badge-secondary">《待辦事項》</span></h1>
+        <div className="alert alert-success ml-3" role="alert"> Hi, {nickName}.   歡迎使用！ </div>
+
+        <div className="alert alert-info ml-3" role="alert">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" value={newTodo} placeholder="請輸入新的待辦事項" aria-label="請輸入新的待辦事項" aria-describedby="button-addon2" onChange={(e) => setNewTodo(e.target.value)} />
+            <div className="input-group-append">
+              <button className="btn btn-info btn-sm" type="button" id="button-addon2" onClick={handleAddTodo}>新增</button>
+            </div>
+          </div>
+          <ul className="list-group">
+            {todo.map((item) => {
+              return (
+                <li className= {item.status 
+                  ? "list-group-item  d-flex justify-content-between align-items-center list-group-item-secondary" 
+                  : "list-group-item  d-flex justify-content-between align-items-center"}  key={item.id
+                  }
+                  onClick={() => {
+                    setEditId(item.id)
+                    setNewContent(item.content)
+                  }}
+                  onBlur={() => setEditId(0)}>
+                  {(editId === item.id) ? <input type="text" class="form-control-sm" value={newContent} onChange={(e) => setNewContent(e.target.value)} onBlur={() => {
+                    setEditId(0)
+                    // console.log(newContent)
+                  }} autoFocus /> :
+                    <span style={ {"textDecoration" : item.status ? "line-through" : "none"}} >
+                      {item.content}
+                    </span>
+                  }
+                  <div className="btn-group">
+                    <button type="button" className={item.status ? "btn btn-outline-secondary btn-sm" : "btn btn-outline-info btn-sm"} onClick={() => handleToggle(item.id)}> {item.status ? '↶' : '✓'}</button>
+                    <button type="button" className={item.status ? "btn btn-outline-secondary btn-sm" : "btn btn-outline-info btn-sm"} onClick={() => handleUpdate(item.id, newContent)}>更新</button>
+                    <button type="button" className={item.status ? "btn btn-outline-secondary btn-sm" : "btn btn-outline-info btn-sm"} onClick={() => handleDelete(item.id)}>刪除</button>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+
+        <div className="alert alert-warning my-4 ml-3" role="alert" style={{ "whiteSpace": "pre-line", "overflowWrap": "break-word" }}>
+          執行結果：<br /> {msg} <br />
+        </div>
+
+      </div>      
     </>
   )
 }
